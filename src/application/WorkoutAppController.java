6b98a6b3143582a31ab535fe4105cd2ef1465f22
KatieBurgess <katherine.burgess@ucalgary.ca>
2022-11-12 22:53:43 +0000

    Scene Change for logging workouts and goals

--- src/application/WorkoutAppController.java
+++ src/application/WorkoutAppController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class WorkoutAppController {
@@ -29,7 +30,11 @@ public class WorkoutAppController {
     }
     
    // Returning user main interface 
-   void returningUser(Scene mainScene) {
+   void userWorkoutLog(Scene mainScene) {
+	   
+   }
+   
+   void userGoalLog(Scene mainScene) {
 	   
    }
     
@@ -42,6 +47,8 @@ public class WorkoutAppController {
     	
     	// Change the scene to add a new user to the ChoiceBox
     	if (user.equals("Add a User") ) {
+    		applicationStage.setTitle("Get Fit");
+    		
     		HBox newUserContainer = new HBox();
     		Label newUserLabel = new Label("Add a New User");
     		TextField newUserTextfield = new TextField();
@@ -56,16 +63,32 @@ public class WorkoutAppController {
     		applicationStage.setScene(addUserScene); // places the new scene on the stage
     		
     	} else {
+    		// Any returning users already in the ChoiceBox
     		applicationStage.setTitle("Joe");
     		
-    		HBox returnUserContainer = new HBox();
+    		VBox returnUserContainer = new VBox();
     		Label returnUserLabel = new Label("Welcome Joe"); // this will change will change based on what user is chosen
+    		Label activityLabel = new Label("How were you active today?");
+    		
+    		HBox workoutContainer = new HBox();
+    		Label logWorkoutLabel = new Label("Log your new workout");
+    		Button doneButton = new Button("Enter Here");
+    		doneButton.setOnAction(doneEvent -> userWorkoutLog(mainScene));
+    		workoutContainer.getChildren().addAll(logWorkoutLabel, doneButton);
+    		
+    		HBox workoutGoalsContainer = new HBox();
+    		Label logGoalsLabel = new Label("Log your workout goals");
+    		Button goalsButton = new Button("Enter Goals Here");
+    		goalsButton.setOnAction(goalsEvent -> userGoalLog(mainScene));
+    		// display the workout goals on the returning users scene
+    		workoutGoalsContainer.getChildren().addAll(logGoalsLabel, goalsButton);
     		
     		
-    		Scene returnUserScene = new Scene(new Label("Placeholder. Add widgets later"));
+    		returnUserContainer.getChildren().addAll(returnUserLabel, activityLabel, workoutContainer, workoutGoalsContainer);
+    		Scene returnUserScene = new Scene(returnUserContainer);
     		applicationStage.setScene(returnUserScene);
     	}
-    	System.out.println("user chosen:" + user);
+    	
     	
     	
     }
