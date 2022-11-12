74e5767f4b3051f78677405994bc2ca2ffbce3e5
KatieBurgess <katherine.burgess@ucalgary.ca>
2022-11-12 21:56:51 +0000

    Addition of scene change: new scene for adding a user

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,11 +11,16 @@ import javafx.scene.layout.VBox;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) { // holds the stage for the window
 		try {
 			
 			FXMLLoader loader = new FXMLLoader();
 			VBox root = loader.load(new FileInputStream("src/application/ProjectWorkoutApp.fxml"));
+			// main class gives the stage to the controller
+			WorkoutAppController controller = (WorkoutAppController)loader.getController();
+			controller.applicationStage = primaryStage; // gives the stage to the controller
+			
+			
 			Scene scene = new Scene(root,400,400);
 			
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
--- src/application/WorkoutAppController.java
+++ src/application/WorkoutAppController.java
@@ -2,19 +2,36 @@ package application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class WorkoutAppController {
-
+	Stage applicationStage;
+	
     @FXML
     private ChoiceBox<String> chooseUserChoiceBox;
 
+    
+    /** 
+     * This method will
+     * 
+     * @param event 
+     */
     @FXML
     void chooseUser(ActionEvent event) {
     	// Get the input from the user 
     	String user = chooseUserChoiceBox.getValue();
     	
+    	// Change the scene to add a new user to the ChoiceBox
+    	if (user.equals("Add a User") ) {
+    		Scene addUserScene = new Scene(new Label("Placeholder. Add widgets later"));
+    		applicationStage.setScene(addUserScene); // places the new scene on the stage
+    	}
     	System.out.println("user chosen:" + user);
+    	
+    	
     }
 
 }
